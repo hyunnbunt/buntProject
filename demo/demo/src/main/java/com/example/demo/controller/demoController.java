b93687c6c416e2fe1d943271380ca6b48410801b
unknown <rha542@gmail.com>
2023-08-10 22:26:23 +0900

    add demoController

--- demo/demo/src/main/java/com/example/demo/controller/demoController.java
+++ demo/demo/src/main/java/com/example/demo/controller/demoController.java
@@ -1,4 +1,14 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+@Slf4j
 public class demoController {
+    @GetMapping("/dogs")
+    public String showDogs() {
+        return "dogs/showDogs";
+    }
 }
