2d2f4ed45344c0ddb6c268e75de58fb6b593c79c
unknown <rha542@gmail.com>
2023-08-16 16:35:47 +0900

    added eventapicontroller and succeed to load the data

--- demo/demo/src/main/java/com/example/demo/api/EventApiController.java
+++ demo/demo/src/main/java/com/example/demo/api/EventApiController.java
@@ -1,4 +1,35 @@
 package com.example.demo.api;
 
+import com.example.demo.dto.EventDto;
+import com.example.demo.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class EventApiController {
+
+    private EventService eventService;
+
+    @Autowired
+    public EventApiController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/events")
+    public List<EventDto> showEvents() {
+        return eventService.showEvents();
+    }
+
+    // RestAPI
+    //    GET (/events) : see all today’s events
+    //    POST (/events) : create a new event (body : organizer_Id = Dog.id}
+    //    GET (/events/{eventId}) : see the detail of the event
+    // PATCH (/dogs/{dogId}) : add an event to participating event column
+
+
 }
--- demo/demo/src/main/java/com/example/demo/controller/EventController.java
+++ demo/demo/src/main/java/com/example/demo/controller/EventController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.dto.EventDto;
 import com.example.demo.service.DogService;
 import com.example.demo.service.EventService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,19 +12,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
+@Slf4j
 public class EventController {
 
     private EventService eventService;
     private DogService dogService;
 
+    @Autowired
     public EventController(EventService eventService, DogService dogService) {
         this.eventService = eventService;
         this.dogService = dogService;
     }
 
-    @GetMapping("/bunt-project/events")
+    @GetMapping("bunt-project/events")
     public String showEvents(Model model) {
         List<EventDto> eventDtoList = eventService.showEvents();
+        if (eventDtoList.isEmpty()) {
+            log.info("No events");
+            return "events/noEvents/";
+        }
         model.addAttribute("events", eventDtoList);
         return "events/showEvents/";
     }
